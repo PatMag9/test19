cf9b429e974885bb115066718c3b2fa3c13032a8
student <magierapatryk@test>
2023-11-23 21:05:28 +0100

    aas

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello sssssuiuouioorld!\n";
+    std::cout << "Hello sssssasdasuiuouioorld!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
